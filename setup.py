f068610a186317fa9b9751b30d5bda5c772ac43c
Jason Neal <jason.neal@astro.up.pt>
2017-05-17 08:59:38 +0100

    fix supported version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Natural Language :: English',
     ],
 
